11d16ed008dd62362d31a680558439ea02f726af
m4ce-w1ndu <16584672+m4ce-w1ndu@users.noreply.github.com>
2026-04-03 04:29:39 +0200

    Update eject.rs to add dismount_volume(). Add todo comment to get_device_node()

--- src/eject.rs
+++ src/eject.rs
@@ -1,6 +1,9 @@
 use windows::Win32::{
     Foundation::HANDLE,
-    System::{IO::DeviceIoControl, Ioctl::FSCTL_LOCK_VOLUME},
+    System::{
+        IO::DeviceIoControl,
+        Ioctl::{FSCTL_DISMOUNT_VOLUME, FSCTL_LOCK_VOLUME},
+    },
 };
 
 use crate::drives::RemovableDrive;
@@ -32,8 +35,29 @@ fn lock_volume(handle: HANDLE) -> Result<(), EjectError> {
     }
 }
 
+/// Dismounts the volume using DeviceIoControl.
 fn dismount_volume(handle: HANDLE) -> Result<(), EjectError> {
-    todo!()
+    let mut bytes_returned: u32 = 0;
+
+    // Call DeviceIoControl without buffers and FSCTL_DISMOUNT_VOLUME
+    let result = unsafe {
+        DeviceIoControl(
+            handle,
+            FSCTL_DISMOUNT_VOLUME,
+            None,
+            0,
+            None,
+            0,
+            Some(&mut bytes_returned),
+            None,
+        )
+    };
+
+    if result.is_err() {
+        Err(EjectError::DismountFailed)
+    } else {
+        Ok(())
+    }
 }
 
 fn get_device_node(mount_point: &str) -> Result<u32, EjectError> {
@@ -44,6 +68,6 @@ fn get_parent_node(devinst: u32) -> Result<u32, EjectError> {
     todo!()
 }
 
-fn request_eject(devinst: u32) -> Result<u32, EjectError> {
+fn request_eject(devinst: u32) -> Result<(), EjectError> {
     todo!()
 }
