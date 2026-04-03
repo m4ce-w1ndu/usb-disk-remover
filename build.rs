a71547ccf0b70ed1ac48ff0641fd93bb512c7c61
m4ce-w1ndu <16584672+m4ce-w1ndu@users.noreply.github.com>
2026-04-03 03:55:22 +0200

    Temporary re-enable the console

--- build.rs
+++ build.rs
@@ -3,6 +3,6 @@ fn main() {
     let _ = embed_resource::compile("app.rc", embed_resource::NONE);
 
     // Tell the linker to use the Windows subsystem (no console window)
-    println!("cargo:rustc-link-arg=/SUBSYSTEM:WINDOWS");
-    println!("cargo:rustc-link-arg=/ENTRY:mainCRTStartup");
+    // println!("cargo:rustc-link-arg=/SUBSYSTEM:WINDOWS");
+    // println!("cargo:rustc-link-arg=/ENTRY:mainCRTStartup");
 }
